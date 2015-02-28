909ab72be27eef8d96c4510e3d46a1a0af857d43
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-03-01 06:34:17 +0900

    add installer

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+set -e
+set -x
+
+tmpdir=`mktemp -d`
+trap "
+set +e
+rm $tmpdir/Rakefile
+rmdir $tmpdir
+" 0
+
+cd $tmpdir
+
+## download Rakefile (https://github.com/minimum2scp/ruby-binary/blob/master/Rakefile)
+curl -L -o Rakefile https://raw.githubusercontent.com/minimum2scp/ruby-binary/master/Rakefile
+
+## install ruby binary by rake task
+## empty tag means latest release
+rake 'install:github_release:install_all[]'
+
