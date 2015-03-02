1b5da133a3f9dc9aa9f8e48fe6fba374184420f8
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-03-03 03:06:48 +0900

    add tag for install:github_release:install_all task

--- install.sh
+++ install.sh
@@ -12,10 +12,12 @@ rmdir $tmpdir
 
 cd $tmpdir
 
+tag=$1
+
 ## download Rakefile (https://github.com/minimum2scp/ruby-binary/blob/master/Rakefile)
 curl -L -o Rakefile https://raw.githubusercontent.com/minimum2scp/ruby-binary/master/Rakefile
 
 ## install ruby binary by rake task
 ## empty tag means latest release
-rake 'install:github_release:install_all[]'
+rake "install:github_release:install_all[${tag}]"
 
