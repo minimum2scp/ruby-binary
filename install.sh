6b76a8feba1e863d8cc93ff5e3a4f206813828b4
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-03-20 02:31:53 +0900

    auto detect platform in install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,25 @@ tag=$1
 ## download Rakefile (https://github.com/minimum2scp/ruby-binary/blob/master/Rakefile)
 curl -L -o Rakefile https://raw.githubusercontent.com/minimum2scp/ruby-binary/master/Rakefile
 
+## detect platform
+arch=`dpkg-architecture -q DEB_HOST_ARCH`
+if [ $arch != 'amd64' ]; then
+  echo "DEB_HOST_ARCH ${arch} is not supported"
+  exit 1
+fi
+
+version_id=`sh -c '. /etc/os-release && echo $VERSION_ID'`
+case $version_id in
+  7)
+    platform=wheezy-${arch};;
+  8)
+    platform=sid-${arch};;
+  *)
+    echo "VERSION_ID ${version_id} is not supported"
+esac
+unset arch version_id
+
 ## install ruby binary by rake task
 ## empty tag means latest release
-rake "install:github_release:install_all[${tag}]"
+rake "install:github_release:install_all[${tag},${platform}]"
 
