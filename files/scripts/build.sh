5d344b27cd543f1011638037c1b4e6fd70256b11
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-03-20 01:41:05 +0900

    update packages before build

--- files/scripts/build.sh
+++ files/scripts/build.sh
@@ -6,9 +6,12 @@ version=$1
 tarball=$2
 log=$3
 
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get dist-upgrade -y
+unset DEBIAN_FRONTEND
+
 rbenv update
-
 rbenv install -v ${version} | tee $log
-
 tar cfz ${tarball} -C /opt/rbenv/versions ${version}
 
