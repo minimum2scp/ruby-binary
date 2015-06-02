559190fe7b67a1d4abe26e0a716650d1509022fd
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-06-03 02:12:38 +0900

    install bundler 1.9.9 by rbenv-default-gems

--- files/scripts/build.sh
+++ files/scripts/build.sh
@@ -11,8 +11,21 @@ apt-get update
 apt-get dist-upgrade -y
 unset DEBIAN_FRONTEND
 
+## update rbenv
 bash -lc "rbenv update"
+
+## update default-gems
+default_gems=`mktemp`
+cat <<DEFAULT_GEMS > $default_gems
+bundler 1.9.9
+pry
+DEFAULT_GEMS
+install -m 644 -o root -g root -p $default_gems ${RBENV_ROOT}/default-gems
+rm $default_gems
+
+## build and install ruby
 bash -lc "rbenv install -v ${version}" | tee $log
 
+## archive binary
 tar cfz ${tarball} -C /opt/rbenv/versions ${version}
 
