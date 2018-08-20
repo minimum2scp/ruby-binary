1e2c9097c768114b3f8440150e87cc019836be25
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2018-08-20 23:11:06 +0900

    bundler 1.16.4

--- spec/stretch-amd64_2.5.1_spec.rb
+++ spec/stretch-amd64_2.5.1_spec.rb
@@ -15,7 +15,7 @@ describe "platform=stretch-amd64 version=2.5.1" do
 
   describe command("RBENV_VERSION=2.5.1 bundle -v") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "Bundler version 1.16.3\n" }
+    its(:stdout){ should eq "Bundler version 1.16.4\n" }
   end
 
   describe command("RBENV_VERSION=2.5.1 gem list") do
