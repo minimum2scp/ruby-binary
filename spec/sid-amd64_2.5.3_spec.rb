a1df6c42ae8a181c097b30fa4937aec3247970ec
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-01-03 21:17:23 +0900

    bundler 2.0.0

--- spec/sid-amd64_2.5.3_spec.rb
+++ spec/sid-amd64_2.5.3_spec.rb
@@ -15,7 +15,7 @@ describe "platform=sid-amd64 version=2.5.3" do
 
   describe command("RBENV_VERSION=2.5.3 bundle -v") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "Bundler version 1.17.3\n" }
+    its(:stdout){ should eq "Bundler version 2.0.0\n" }
   end
 
   describe command("RBENV_VERSION=2.5.3 gem list") do
