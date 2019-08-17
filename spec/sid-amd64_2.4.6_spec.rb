d78014f035d736327a8fcb94fed7e421781a2d2f
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-08-17 23:05:20 +0900

    rubygems 3.0.6

--- spec/sid-amd64_2.4.6_spec.rb
+++ spec/sid-amd64_2.4.6_spec.rb
@@ -10,7 +10,7 @@ describe "platform=sid-amd64 version=2.4.6" do
 
   describe command("RBENV_VERSION=2.4.6 gem -v") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "3.0.4\n" }
+    its(:stdout){ should eq "3.0.6\n" }
   end
 
   describe command("RBENV_VERSION=2.4.6 bundle -v") do
