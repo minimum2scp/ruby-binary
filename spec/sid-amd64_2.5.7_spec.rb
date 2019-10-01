5d8c387afac27586c5929c4acfa8265a48ddc193
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-10-02 00:30:14 +0900

    Ruby 2.6.5, 2.5.7, 2.4.8

---
+++ spec/sid-amd64_2.5.7_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+set :docker_image, "minimum2scp/ruby-binary:test_sid-amd64_2.5.7"
+
+describe "platform=sid-amd64 version=2.5.7" do
+  describe command("RBENV_VERSION=2.5.7 ruby -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "ruby 2.5.7p206 (2019-10-01 revision 67816) [x86_64-linux]\n" }
+  end
+
+  describe command("RBENV_VERSION=2.5.7 gem -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "3.0.6\n" }
+  end
+
+  describe command("RBENV_VERSION=2.5.7 bundle -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "Bundler version 2.0.2\n" }
+  end
+
+  describe command("RBENV_VERSION=2.5.7 gem list") do
+    let(:login_shell){ true }
+    its(:stdout){ should match /^pry \(/ }
+  end
+
+  describe command("RBENV_VERSION=2.5.7 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
+    let(:login_shell){ true }
+    its(:stdout){ should match /^OpenSSL 1\.1\.1/ }
+  end
+
+  describe command("RBENV_VERSION=2.5.7 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "yes\n" }
+  end
+end
+
