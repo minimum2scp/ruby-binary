231cca0dc10e6894aac850e1bb3900e86c18a9fd
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-10-26 01:09:48 +0900

    Build ruby 2.4.9, 2.5.7, 2.7.0-preview2 on buster-amd64, stretch-amd64

---
+++ spec/buster-amd64_2.4.9_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+set :docker_image, "minimum2scp/ruby-binary:test_buster-amd64_2.4.9"
+
+describe "platform=buster-amd64 version=2.4.9" do
+  describe command("RBENV_VERSION=2.4.9 ruby -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "ruby 2.4.9p362 (2019-10-02 revision 67824) [x86_64-linux]\n" }
+  end
+
+  describe command("RBENV_VERSION=2.4.9 gem -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "3.0.6\n" }
+  end
+
+  describe command("RBENV_VERSION=2.4.9 bundle -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "Bundler version 2.0.2\n" }
+  end
+
+  describe command("RBENV_VERSION=2.4.9 gem list") do
+    let(:login_shell){ true }
+    its(:stdout){ should match /^pry \(/ }
+  end
+
+  describe command("RBENV_VERSION=2.4.9 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
+    let(:login_shell){ true }
+    its(:stdout){ should match /^OpenSSL 1\.1\.1/ }
+  end
+
+  describe command("RBENV_VERSION=2.4.9 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "yes\n" }
+  end
+end
+
