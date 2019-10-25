bb671fe01d39115bee1f24b22e2cf78438c2ee7b
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-10-26 02:30:53 +0900

    Added test for /etc/debian_version file

--- spec/stretch-amd64_2.5.7_spec.rb
+++ spec/stretch-amd64_2.5.7_spec.rb
@@ -3,6 +3,10 @@ require "spec_helper"
 set :docker_image, "minimum2scp/ruby-binary:test_stretch-amd64_2.5.7"
 
 describe "platform=stretch-amd64 version=2.5.7" do
+  describe file('/etc/debian_version') do
+    its(:content){ should match a_string_starting_with('9.') }
+  end
+
   describe command("RBENV_VERSION=2.5.7 ruby -v") do
     let(:login_shell){ true }
     its(:stdout){ should eq "ruby 2.5.7p206 (2019-10-01 revision 67816) [x86_64-linux]\n" }
