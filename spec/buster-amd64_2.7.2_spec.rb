86540820500fb82065f59b9ab54148b61e95b457
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2020-12-11 02:37:22 +0900

    Rewrite spec with shared examples

--- spec/buster-amd64_2.7.2_spec.rb
+++ spec/buster-amd64_2.7.2_spec.rb
@@ -3,38 +3,12 @@ require "spec_helper"
 set :docker_image, "minimum2scp/ruby-binary:test_buster-amd64_2.7.2"
 
 describe "platform=buster-amd64 version=2.7.2" do
-  describe file('/etc/debian_version') do
-    its(:content){ should match a_string_starting_with('10.') }
-  end
-
-  describe command("RBENV_VERSION=2.7.2 ruby -v") do
-    let(:login_shell){ true }
-    its(:stdout){ should eq "ruby 2.7.2p137 (2020-10-01 revision 5445e04352) [x86_64-linux]\n" }
-  end
-
-  describe command("RBENV_VERSION=2.7.2 gem -v") do
-    let(:login_shell){ true }
-    its(:stdout){ should eq "3.2.0\n" }
-  end
-
-  describe command("RBENV_VERSION=2.7.2 gem list --exact bundler") do
-    let(:login_shell){ true }
-    its(:stdout){ should eq "bundler (default: 2.2.0, 1.17.3)\n" }
-  end
-
-  describe command("RBENV_VERSION=2.7.2 gem list") do
-    let(:login_shell){ true }
-    its(:stdout){ should match /^pry \(/ }
-  end
+  it_behaves_like 'debian buster'
+  it_behaves_like 'ruby 2.7.2'
 
   describe command("RBENV_VERSION=2.7.2 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
     let(:login_shell){ true }
-    its(:stdout){ should match /^OpenSSL 1\.1\.1/ }
-  end
-
-  describe command("RBENV_VERSION=2.7.2 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
-    let(:login_shell){ true }
-    its(:stdout){ should eq "yes\n" }
+    its(:stdout){ should start_with('OpenSSL 1.1.1') }
   end
 end
 
---
+++ spec/support/shared_examples/debian_version.rb
@@ -0,0 +1,18 @@
+RSpec.shared_examples 'debian sid' do
+  describe file('/etc/debian_version') do
+    its(:content){ should eq "bullseye/sid\n" }
+  end
+end
+
+RSpec.shared_examples 'debian buster' do
+  describe file('/etc/debian_version') do
+    its(:content){ should match a_string_starting_with('10.') }
+  end
+end
+
+RSpec.shared_examples 'debian stretch' do
+  describe file('/etc/debian_version') do
+    its(:content){ should match a_string_starting_with('9.') }
+  end
+end
+
