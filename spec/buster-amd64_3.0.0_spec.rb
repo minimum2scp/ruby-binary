0a9e2b5f8909e43a875f73998d47c8169a993508
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2020-12-25 19:31:04 +0900

    Updated spec for ruby 3.0.0

---
+++ spec/buster-amd64_3.0.0_spec.rb
@@ -0,0 +1,8 @@
+set :docker_image, "minimum2scp/ruby-binary:test_buster-amd64_3.0.0"
+
+describe "platform=buster-amd64 version=3.0.0" do
+  include_context 'openssl 1.1.1'
+  it_behaves_like 'debian buster'
+  it_behaves_like 'ruby 3.0.0'
+end
+
