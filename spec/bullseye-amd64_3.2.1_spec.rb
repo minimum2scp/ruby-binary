41fe63073a4da665266307f4ef716045b73fb620
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2023-02-09 09:59:34 +0900

    Updated spec

---
+++ spec/bullseye-amd64_3.2.1_spec.rb
@@ -0,0 +1,8 @@
+set :docker_image, "minimum2scp/ruby-binary:test_bullseye-amd64_3.2.1"
+
+describe "platform=bullseye-amd64 version=3.2.1" do
+  include_context 'openssl 1.1.1'
+  it_behaves_like 'debian bullseye'
+  it_behaves_like 'ruby 3.2.1'
+end
+
