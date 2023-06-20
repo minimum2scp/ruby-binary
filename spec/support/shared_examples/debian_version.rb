06bea79af3bdbdf1896dbc71fb8bf86766b0677a
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2023-06-21 00:40:54 +0900

    Skip test about /etc/debian_version on debian sid

--- spec/support/shared_examples/debian_version.rb
+++ spec/support/shared_examples/debian_version.rb
@@ -1,5 +1,5 @@
 RSpec.shared_examples 'debian sid' do
-  describe file('/etc/debian_version') do
+  xdescribe file('/etc/debian_version') do
     its(:content){ should match a_string_starting_with('12.') }
   end
 end
