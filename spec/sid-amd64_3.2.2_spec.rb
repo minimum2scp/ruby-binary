51d69ad84340db3fb8dcc4b11b6da7e6fc3ce48e
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2023-10-20 13:31:31 +0900

    Enforce system openssl (3.0 series) on sid ruby 3.1, 3.2

--- spec/sid-amd64_3.2.2_spec.rb
+++ spec/sid-amd64_3.2.2_spec.rb
@@ -1,7 +1,7 @@
 set :docker_image, "minimum2scp/ruby-binary:test_sid-amd64_3.2.2"
 
 describe "platform=sid-amd64 version=3.2.2" do
-  include_context 'openssl 3.1.3'
+  include_context 'openssl 3.0.11'
   it_behaves_like 'debian sid'
   it_behaves_like 'ruby 3.2.2'
 end
