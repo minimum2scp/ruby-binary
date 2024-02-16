f08e9adb28eabd2401fd3960f507cff4b23fc8d0
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2024-02-16 19:19:40 +0900

    openssl is updated to 3.1.5 on sid

--- spec/sid-amd64_3.2.3_spec.rb
+++ spec/sid-amd64_3.2.3_spec.rb
@@ -1,7 +1,7 @@
 set :docker_image, "minimum2scp/ruby-binary:test_sid-amd64_3.2.3"
 
 describe "platform=sid-amd64 version=3.2.3" do
-  include_context 'openssl 3.1.4'
+  include_context 'openssl 3.1.5'
   it_behaves_like 'debian sid'
   it_behaves_like 'ruby 3.2.3'
 end
