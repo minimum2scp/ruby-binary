efca8019e38f7985fdafde046eb0b6e9f428bad4
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2024-06-17 00:26:23 +0900

    Updated spec

--- spec/support/shared_examples/ruby.rb
+++ spec/support/shared_examples/ruby.rb
@@ -1,39 +1,39 @@
-RSpec.shared_examples 'ruby 3.3.2' do
-  describe command("RBENV_VERSION=3.3.2 ruby -v") do
+RSpec.shared_examples 'ruby 3.3.3' do
+  describe command("RBENV_VERSION=3.3.3 ruby -v") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "ruby 3.3.2 (2024-05-30 revision e5a195edf6) [x86_64-linux]\n" }
+    its(:stdout){ should eq "ruby 3.3.3 (2024-06-12 revision f1c7b6f435) [x86_64-linux]\n" }
   end
 
-  describe command("RBENV_VERSION=3.3.2 gem -v") do
+  describe command("RBENV_VERSION=3.3.3 gem -v") do
     let(:login_shell){ true }
     its(:stdout){ should eq "3.5.11\n" }
   end
 
-  describe command("RBENV_VERSION=3.3.2 gem list --exact bundler") do
+  describe command("RBENV_VERSION=3.3.3 gem list --exact bundler") do
     let(:login_shell){ true }
     its(:stdout){ should eq "bundler (default: 2.5.11)\n" }
   end
 
-  describe command("RBENV_VERSION=3.3.2 gem list") do
+  describe command("RBENV_VERSION=3.3.3 gem list") do
     let(:login_shell){ true }
     its(:stdout){ should match /^pry \(/ }
   end
 
-  describe command("RBENV_VERSION=3.3.2 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
+  describe command("RBENV_VERSION=3.3.3 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
     let(:login_shell){ true }
     its(:stdout){ should eq "yes\n" }
   end
 
-  describe file("/opt/rbenv/versions/3.3.2/openssl") do
+  describe file("/opt/rbenv/versions/3.3.3/openssl") do
     it { should_not exist }
   end
 
-  describe command("RBENV_VERSION=3.3.2 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
+  describe command("RBENV_VERSION=3.3.3 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
     let(:login_shell){ true }
     its(:stdout){ should start_with("OpenSSL #{openssl_version}") }
   end
 
-  describe command("RBENV_VERSION=3.3.2 ruby --yjit -e 'p RubyVM::YJIT.enabled?'") do
+  describe command("RBENV_VERSION=3.3.3 ruby --yjit -e 'p RubyVM::YJIT.enabled?'") do
     let(:login_shell){ true }
     its(:stdout){ should eq "true\n" }
     its(:stderr){ should eq '' }
