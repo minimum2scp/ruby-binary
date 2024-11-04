c7aba74aa0e812ee20a9036e3bb977f13c23e2f7
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2024-11-05 00:32:51 +0900

    Updated spec

---
+++ spec/bookworm-amd64_3.3.6_spec.rb
@@ -0,0 +1,8 @@
+set :docker_image, "minimum2scp/ruby-binary:test_bookworm-amd64_3.3.6"
+
+describe "platform=bookworm-amd64 version=3.3.6" do
+  include_context 'openssl 3.0.14'
+  it_behaves_like 'debian bookworm'
+  it_behaves_like 'ruby 3.3.6'
+end
+
--- spec/buster-amd64_3.3.5_spec.rb
+++
@@ -1,8 +0,0 @@
-set :docker_image, "minimum2scp/ruby-binary:test_buster-amd64_3.3.5"
-
-describe "platform=buster-amd64 version=3.3.5" do
-  include_context 'openssl 1.1.1'
-  it_behaves_like 'debian buster'
-  it_behaves_like 'ruby 3.3.5'
-end
-
