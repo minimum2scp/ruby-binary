80352c0ebeb9457065a53f4186041baac8e49d20
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2024-11-15 19:32:37 +0900

    openssl is updated to 3.0.15 on bookworm

--- spec/bookworm-amd64_3.2.6_spec.rb
+++ spec/bookworm-amd64_3.2.6_spec.rb
@@ -1,7 +1,7 @@
 set :docker_image, "minimum2scp/ruby-binary:test_bookworm-amd64_3.2.6"
 
 describe "platform=bookworm-amd64 version=3.2.6" do
-  include_context 'openssl 3.0.14'
+  include_context 'openssl 3.0.15'
   it_behaves_like 'debian bookworm'
   it_behaves_like 'ruby 3.2.6'
 end
