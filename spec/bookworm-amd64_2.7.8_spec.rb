dc78263c0b049ff5ca78f01b0518923147d91655
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-03-27 23:12:46 +0900

    Removed ruby 2.7

--- spec/bookworm-amd64_2.7.8_spec.rb
+++
@@ -1,8 +0,0 @@
-set :docker_image, "minimum2scp/ruby-binary:test_bookworm-amd64_2.7.8"
-
-describe "platform=bookworm-amd64 version=2.7.8" do
-  include_context 'openssl 1.1.1'
-  it_behaves_like 'debian bookworm'
-  it_behaves_like 'ruby 2.7.8'
-end
-
