860e09d2c4277fcf94b958f42244af05806a0e43
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-04-09 21:13:20 +0900

    openssl is updated to 3.5.0 on sid

--- spec/support/shared_contexts/openssl.rb
+++ spec/support/shared_contexts/openssl.rb
@@ -6,7 +6,7 @@ RSpec.shared_context 'openssl 3.0.15' do
   let(:openssl_version){ '3.0.15' }
 end
 
-RSpec.shared_context 'openssl 3.4.1' do
-  let(:openssl_version){ '3.4.1' }
+RSpec.shared_context 'openssl 3.5.0' do
+  let(:openssl_version){ '3.5.0' }
 end
 
