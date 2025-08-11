1d74de14df829daab341b3d8cc07ef7e06280f4a
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-08-12 08:41:54 +0900

    Fix bookworm build: openssl 3.0.17

--- spec/support/shared_contexts/openssl.rb
+++ spec/support/shared_contexts/openssl.rb
@@ -2,8 +2,8 @@ RSpec.shared_context 'openssl 1.1.1' do
   let(:openssl_version){ '1.1.1' }
 end
 
-RSpec.shared_context 'openssl 3.0.16' do
-  let(:openssl_version){ '3.0.16' }
+RSpec.shared_context 'openssl 3.0.17' do
+  let(:openssl_version){ '3.0.17' }
 end
 
 RSpec.shared_context 'openssl 3.5.2' do
