f02b2376869d3704583f9dd3bf004733bf9b9864
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-08-12 08:41:54 +0900

    Fix bookworm build: openssl 3.0.17

--- spec/bookworm-amd64_3.3.9_spec.rb
+++ spec/bookworm-amd64_3.3.9_spec.rb
@@ -1,7 +1,7 @@
 set :docker_image, "minimum2scp/ruby-binary:test_bookworm-amd64_3.3.9"
 
 describe "platform=bookworm-amd64 version=3.3.9" do
-  include_context 'openssl 3.0.16'
+  include_context 'openssl 3.0.17'
   it_behaves_like 'debian bookworm'
   it_behaves_like 'ruby 3.3.9'
 end
