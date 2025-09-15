57c46a6019f9d61aca572c6a3a008bba56d68efa
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-09-15 23:25:02 +0900

    Removed ruby 3.1

--- spec/sid-amd64_3.1.7_spec.rb
+++
@@ -1,8 +0,0 @@
-set :docker_image, "minimum2scp/ruby-binary:test_sid-amd64_3.1.7"
-
-describe "platform=sid-amd64 version=3.1.7" do
-  include_context 'openssl 3.5.2'
-  it_behaves_like 'debian sid'
-  it_behaves_like 'ruby 3.1.7'
-end
-
