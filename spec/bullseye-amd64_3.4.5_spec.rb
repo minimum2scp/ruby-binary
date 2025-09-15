3f247a3149811ef3cd5f701daaea68b616bead95
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-09-15 23:08:50 +0900

    Removed debian 11 bullseye support

--- spec/bullseye-amd64_3.4.5_spec.rb
+++
@@ -1,8 +0,0 @@
-set :docker_image, "minimum2scp/ruby-binary:test_bullseye-amd64_3.4.5"
-
-describe "platform=bullseye-amd64 version=3.4.5" do
-  include_context 'openssl 1.1.1'
-  it_behaves_like 'debian bullseye'
-  it_behaves_like 'ruby 3.4.5'
-end
-
