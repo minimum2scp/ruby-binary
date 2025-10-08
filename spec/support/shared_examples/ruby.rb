aabf9e0099037ae39f07f34a8c8e47faa1aeba0d
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-10-08 23:04:11 +0900

    Updated spec

--- spec/support/shared_examples/ruby.rb
+++ spec/support/shared_examples/ruby.rb
@@ -1,34 +1,34 @@
-RSpec.shared_examples 'ruby 3.4.6' do
-  describe command("RBENV_VERSION=3.4.6 ruby -v") do
+RSpec.shared_examples 'ruby 3.4.7' do
+  describe command("RBENV_VERSION=3.4.7 ruby -v") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "ruby 3.4.6 (2025-09-16 revision dbd83256b1) +PRISM [x86_64-linux]\n" }
+    its(:stdout){ should eq "ruby 3.4.7 (2025-10-08 revision 7a5688e2a2) +PRISM [x86_64-linux]\n" }
   end
 
-  describe command("RBENV_VERSION=3.4.6 gem -v") do
+  describe command("RBENV_VERSION=3.4.7 gem -v") do
     let(:login_shell){ true }
     its(:stdout){ should eq "3.7.2\n" }
   end
 
-  describe command("RBENV_VERSION=3.4.6 gem list --exact bundler") do
+  describe command("RBENV_VERSION=3.4.7 gem list --exact bundler") do
     let(:login_shell){ true }
     its(:stdout){ should eq "bundler (default: 2.7.2)\n" }
   end
 
-  describe command("RBENV_VERSION=3.4.6 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
+  describe command("RBENV_VERSION=3.4.7 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
     let(:login_shell){ true }
     its(:stdout){ should eq "yes\n" }
   end
 
-  describe file("/opt/rbenv/versions/3.4.6/openssl") do
+  describe file("/opt/rbenv/versions/3.4.7/openssl") do
     it { should_not exist }
   end
 
-  describe command("RBENV_VERSION=3.4.6 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
+  describe command("RBENV_VERSION=3.4.7 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
     let(:login_shell){ true }
     its(:stdout){ should start_with("OpenSSL #{openssl_version}") }
   end
 
-  describe command("RBENV_VERSION=3.4.6 ruby --yjit -e 'p RubyVM::YJIT.enabled?'") do
+  describe command("RBENV_VERSION=3.4.7 ruby --yjit -e 'p RubyVM::YJIT.enabled?'") do
     let(:login_shell){ true }
     its(:stdout){ should eq "true\n" }
     its(:stderr){ should eq '' }
--- spec/trixie-amd64_3.4.6_spec.rb
+++
@@ -1,8 +0,0 @@
-set :docker_image, "minimum2scp/ruby-binary:test_trixie-amd64_3.4.6"
-
-describe "platform=trixie-amd64 version=3.4.6" do
-  include_context 'openssl 3.5.1'
-  it_behaves_like 'debian trixie'
-  it_behaves_like 'ruby 3.4.6'
-end
-
