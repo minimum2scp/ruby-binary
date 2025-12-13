2f718d526086a5a70c2a2e5e161fdf016d0fe740
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-12-13 16:08:30 +0900

    Fixed trixie build: openssl 3.5.4

--- spec/support/shared_contexts/openssl.rb
+++ spec/support/shared_contexts/openssl.rb
@@ -2,10 +2,6 @@ RSpec.shared_context 'openssl 3.0.17' do
   let(:openssl_version){ '3.0.17' }
 end
 
-RSpec.shared_context 'openssl 3.5.1' do
-  let(:openssl_version){ '3.5.1' }
-end
-
 RSpec.shared_context 'openssl 3.5.4' do
   let(:openssl_version){ '3.5.4' }
 end
