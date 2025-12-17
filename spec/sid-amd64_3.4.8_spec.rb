cc16dc17d092595e453cccb87a87834e87d5bc7e
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-12-17 21:12:55 +0900

    Updated spec

---
+++ spec/sid-amd64_3.4.8_spec.rb
@@ -0,0 +1,8 @@
+set :docker_image, "minimum2scp/ruby-binary:test_sid-amd64_3.4.8"
+
+describe "platform=sid-amd64 version=3.4.8" do
+  include_context 'openssl 3.5.4'
+  it_behaves_like 'debian sid'
+  it_behaves_like 'ruby 3.4.8'
+end
+
