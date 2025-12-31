df589f0db4d96fdec2f27e89ac879ee023dc5fa5
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2025-12-31 22:34:05 +0900

    Added spec for ruby 4.0.0

--- spec/support/shared_examples/ruby.rb
+++ spec/support/shared_examples/ruby.rb
@@ -1,3 +1,40 @@
+RSpec.shared_examples 'ruby 4.0.0' do
+  describe command("RBENV_VERSION=4.0.0 ruby -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "ruby 4.0.0 (2025-12-25 revision 553f1675f3) +PRISM [x86_64-linux]\n" }
+  end
+
+  describe command("RBENV_VERSION=4.0.0 gem -v") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "4.0.3\n" }
+  end
+
+  describe command("RBENV_VERSION=4.0.0 gem list --exact bundler") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "bundler (default: 4.0.3)\n" }
+  end
+
+  describe command("RBENV_VERSION=4.0.0 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "yes\n" }
+  end
+
+  describe file("/opt/rbenv/versions/4.0.0/openssl") do
+    it { should_not exist }
+  end
+
+  describe command("RBENV_VERSION=4.0.0 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
+    let(:login_shell){ true }
+    its(:stdout){ should start_with("OpenSSL #{openssl_version}") }
+  end
+
+  describe command("RBENV_VERSION=4.0.0 ruby --yjit -e 'p RubyVM::YJIT.enabled?'") do
+    let(:login_shell){ true }
+    its(:stdout){ should eq "true\n" }
+    its(:stderr){ should eq '' }
+  end
+end
+
 RSpec.shared_examples 'ruby 3.4.8' do
   describe command("RBENV_VERSION=3.4.8 ruby -v") do
     let(:login_shell){ true }
