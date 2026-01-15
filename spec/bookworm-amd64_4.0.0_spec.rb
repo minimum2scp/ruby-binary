28f73f063f29eb095600e883ee90608214b4dada
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2026-01-15 21:39:33 +0900

    Updated spec

--- spec/bookworm-amd64_4.0.0_spec.rb
+++
@@ -1,8 +0,0 @@
-set :docker_image, "minimum2scp/ruby-binary:test_bookworm-amd64_4.0.0"
-
-describe "platform=bookworm-amd64 version=4.0.0" do
-  include_context 'openssl 3.0.18'
-  it_behaves_like 'debian bookworm'
-  it_behaves_like 'ruby 4.0.0'
-end
-
--- spec/trixie-amd64_3.2.9_spec.rb
+++
@@ -1,8 +0,0 @@
-set :docker_image, "minimum2scp/ruby-binary:test_trixie-amd64_3.2.9"
-
-describe "platform=trixie-amd64 version=3.2.9" do
-  include_context 'openssl 3.5.4'
-  it_behaves_like 'debian trixie'
-  it_behaves_like 'ruby 3.2.9'
-end
-
