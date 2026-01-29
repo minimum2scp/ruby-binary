634fedefec1813460f7af502f16d763b71454e0f
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2026-01-29 23:27:50 +0900

    Fixed sid build: openssl 3.5.5

--- spec/sid-amd64_3.4.8_spec.rb
+++ spec/sid-amd64_3.4.8_spec.rb
@@ -1,7 +1,7 @@
 set :docker_image, "minimum2scp/ruby-binary:test_sid-amd64_3.4.8"
 
 describe "platform=sid-amd64 version=3.4.8" do
-  include_context 'openssl 3.5.4'
+  include_context 'openssl 3.5.5'
   it_behaves_like 'debian sid'
   it_behaves_like 'ruby 3.4.8'
 end
--- spec/sid-amd64_4.0.1_spec.rb
+++ spec/sid-amd64_4.0.1_spec.rb
@@ -1,7 +1,7 @@
 set :docker_image, "minimum2scp/ruby-binary:test_sid-amd64_4.0.1"
 
 describe "platform=sid-amd64 version=4.0.1" do
-  include_context 'openssl 3.5.4'
+  include_context 'openssl 3.5.5'
   it_behaves_like 'debian sid'
   it_behaves_like 'ruby 4.0.1'
 end
