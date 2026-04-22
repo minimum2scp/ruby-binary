86048785e2e0eeba03de44903074ca4a88278f13
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2026-04-22 13:29:01 +0900

    Updated spec

---
+++ spec/bookworm-amd64_4.0.3_spec.rb
@@ -0,0 +1,8 @@
+set :docker_image, "minimum2scp/ruby-binary:test_bookworm-amd64_4.0.3"
+
+describe "platform=bookworm-amd64 version=4.0.3" do
+  include_context 'openssl 3.0.19'
+  it_behaves_like 'debian bookworm'
+  it_behaves_like 'ruby 4.0.3'
+end
+
