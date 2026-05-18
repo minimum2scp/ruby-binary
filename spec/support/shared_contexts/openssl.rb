6c4a0c36c53447625d912dab7d420342a29ca6c7
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2026-05-18 13:22:53 +0900

    Fixed trixie build: openssl 3.5.6

--- spec/support/shared_contexts/openssl.rb
+++ spec/support/shared_contexts/openssl.rb
@@ -2,8 +2,8 @@ RSpec.shared_context 'openssl 3.0.19' do
   let(:openssl_version){ '3.0.19' }
 end
 
-RSpec.shared_context 'openssl 3.5.5' do
-  let(:openssl_version){ '3.5.5' }
+RSpec.shared_context 'openssl 3.5.6' do
+  let(:openssl_version){ '3.5.6' }
 end
 
 RSpec.shared_context 'openssl 3.6.2' do
--- spec/trixie-amd64_3.2.11_spec.rb
+++ spec/trixie-amd64_3.2.11_spec.rb
@@ -1,7 +1,7 @@
 set :docker_image, "minimum2scp/ruby-binary:test_trixie-amd64_3.2.11"
 
 describe "platform=trixie-amd64 version=3.2.11" do
-  include_context 'openssl 3.5.5'
+  include_context 'openssl 3.5.6'
   it_behaves_like 'debian trixie'
   it_behaves_like 'ruby 3.2.11'
 end
