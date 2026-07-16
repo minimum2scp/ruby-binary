ebbe4b45b9b26e7fe1cccc10586db142cc18b8df
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2026-07-17 00:04:59 +0900

    Updated spec

--- spec/support/shared_examples/ruby.rb
+++ spec/support/shared_examples/ruby.rb
@@ -72,37 +72,37 @@ RSpec.shared_examples 'ruby 3.4.10' do
   end
 end
 
-RSpec.shared_examples 'ruby 3.3.11' do
-  describe command("RBENV_VERSION=3.3.11 ruby -v") do
+RSpec.shared_examples 'ruby 3.3.12' do
+  describe command("RBENV_VERSION=3.3.12 ruby -v") do
     let(:login_shell){ true }
-    its(:stdout){ should eq "ruby 3.3.11 (2026-03-26 revision 1f2d15125a) [x86_64-linux]\n" }
+    its(:stdout){ should eq "ruby 3.3.12 (2026-07-16 revision 0581089df9) [x86_64-linux]\n" }
   end
 
-  describe command("RBENV_VERSION=3.3.11 gem -v") do
+  describe command("RBENV_VERSION=3.3.12 gem -v") do
     let(:login_shell){ true }
     its(:stdout){ should eq "4.0.16\n" }
   end
 
-  describe command("RBENV_VERSION=3.3.11 gem list --exact bundler") do
+  describe command("RBENV_VERSION=3.3.12 gem list --exact bundler") do
     let(:login_shell){ true }
     its(:stdout){ should eq "bundler (default: 4.0.16)\n" }
   end
 
-  describe command("RBENV_VERSION=3.3.11 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
+  describe command("RBENV_VERSION=3.3.12 ruby -rrbconfig -e 'puts RbConfig::CONFIG[\"LIBRUBY_RELATIVE\"]'") do
     let(:login_shell){ true }
     its(:stdout){ should eq "yes\n" }
   end
 
-  describe file("/opt/rbenv/versions/3.3.11/openssl") do
+  describe file("/opt/rbenv/versions/3.3.12/openssl") do
     it { should_not exist }
   end
 
-  describe command("RBENV_VERSION=3.3.11 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
+  describe command("RBENV_VERSION=3.3.12 ruby -ropenssl -e 'puts OpenSSL::OPENSSL_VERSION'") do
     let(:login_shell){ true }
     its(:stdout){ should start_with("OpenSSL #{openssl_version}") }
   end
 
-  describe command("RBENV_VERSION=3.3.11 ruby --yjit -e 'p RubyVM::YJIT.enabled?'") do
+  describe command("RBENV_VERSION=3.3.12 ruby --yjit -e 'p RubyVM::YJIT.enabled?'") do
     let(:login_shell){ true }
     its(:stdout){ should eq "true\n" }
     its(:stderr){ should eq '' }
